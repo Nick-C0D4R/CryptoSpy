70918ee4a66023ee376db06f975c8f966e38c1ad
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-02 19:56:45 +0300

    Add CoinCapApiHandler to get json from external API

---
+++ CoinCapHandler/CoinCapApiHandler.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinCapHandler
+{
+    public class CoinCapHandler
+    {
+        private HttpClient _client = new HttpClient();
+        public string Url => "https://api.coincap.io/v2";
+
+        public async Task<JObject> GetJson(string url)
+        {
+            HttpResponseMessage message = await _client.GetAsync(url);
+            if (message.IsSuccessStatusCode)
+            {
+                string response = JsonConvert.SerializeObject(message.Content);
+                return JObject.Parse(response);
+            }
+
+            if ((int)message.StatusCode >= 400 && (int)message.StatusCode <= 417)
+                throw new ArgumentException("Invalid URL");
+            
+            throw new CannotUnloadAppDomainException("Server Error");
+        }
+    }
+}
