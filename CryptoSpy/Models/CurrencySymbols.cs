90b1a72930609748e3adae9c972469d982e2b7e4
Mykola Kovshar <knikolay20031230@gmail.com>
2023-06-02 20:27:05 +0300

    Add enum to save cryptocurrency symbols

---
+++ CryptoSpy/Models/CurrencySymbols.cs
@@ -0,0 +1,26 @@
+﻿namespace CryptoSpy.Models
+{
+    public enum CurrencySymbols
+    {
+        ETH,
+        BTC,
+        USDT,
+        BNB,
+        USDC,
+        XPR,
+        ADA,
+        DOGE,
+        SOL,
+        MATIC,
+        TRX,
+        LTC,
+        DOT,
+        BUSD,
+        SHIB,
+        AVAX,
+        DAI,
+        WBTC,
+        LEO,
+        LINK
+    }
+}
